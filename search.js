4c4e34e75895be831da094f7870ea774011309e1
Thomas Iatrou <40562247+Thomasi13@users.noreply.github.com>
2021-07-31 17:27:11 +0200

    Create search.js

---
+++ search.js
@@ -0,0 +1,21 @@
+
+$('#search-btn').click(function(){
+	var t1 = new TimelineMax({paused:true});
+  	t1.to('#search-transition-block', 0,
+  		{opacity: 1});
+      
+  	t1.to('#search-transition-block', 0.7, 
+   		{scale:70, ease: Power3.easeOut})
+    
+    t1.to('#search-wrapper', 0,
+  		{display:'block'},0)
+      
+  	t1.to('#search-wrapper' , 0.6 ,
+  		{opacity:1,ease: Power3.easeOut},0.5)
+    
+  	t1.play()
+  
+  $("#exit-search").click(function(){
+		t1.reverse();
+	})
+})
