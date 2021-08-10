97f5918818ec0dcce6b5b8d571bd730ed38a6a7d
Thomas Iatrou <40562247+Thomasi13@users.noreply.github.com>
2021-08-10 14:27:02 +0200

    Update scrollMagic_mobile.js

--- scrollMagic_mobile.js
+++ scrollMagic_mobile.js
@@ -4,7 +4,11 @@
 var controller = new ScrollMagic.Controller({vertical: true });
 var header = $("header").innerHeight()
 var footer = $(".navbar").innerHeight()
-var windowHeight = window.innerHeight
+
+function getwindowHeight(slow){
+	var windowheight = window.innerHeight
+	return (windowHeight * slow)
+}
 
 
 
@@ -13,7 +17,7 @@ $('#intro-section').innerHeight(windowHeight - footer - header)
 	
 // ADD CLASS ACTIVE TO SECTION
 $('section').each(function(){
-	var sectionWidth = $(this).innerHeight()
+    var sectionWidth = $(this).innerHeight()
     var href = $(this).attr('id')
     var animEl = "[anchor="+href+"]"
     var scrollBar = "[anchor="+href+"] .scrollbar"
@@ -23,9 +27,9 @@ $('section').each(function(){
     {scaleX: 0 }, {scaleX: 1});
 	
     var scene = new ScrollMagic.Scene({
-  		triggerElement: this,
+  	triggerElement: this,
     	triggerHook : 0.55,
-        duration: sectionWidth,
+        duration: getwindowHeight(1.5),
     	reverse : true,
     	offset:0
   	}) 
@@ -40,12 +44,12 @@ $('section').each(function(){
 *******************/
 // BLOCK IMAGE INTRO
 var tween = TweenMax.staggerFromTo("#block-image-intro", 1, 
-    {backgroundSize: "auto 140%"}, 
-    {backgroundSize:"auto 100%"});
+    {scale: 1.3}, 
+    {scale: 1});
 
 var scene = new ScrollMagic.Scene({
    triggerElement: '#block-image-intro', 
-   duration: windowHeight * 1.5,
+   duration: getwindowHeight(1.5),
    triggerHook : 1,
    reverse : true,
    offset:0
@@ -65,8 +69,8 @@ var tween = TweenMax.staggerFromTo('.verticale-word', 1,
 
 var scene = new ScrollMagic.Scene({
 	triggerElement: "#vertical-section-words",
-	triggerHook : 1,
-  	duration : windowHeight * 1.5,
+	triggerHook : 0.9,
+  	duration : getwindowHeight(1.5)
 })
 .setTween(tween)
 .addTo(controller)
@@ -74,15 +78,14 @@ var scene = new ScrollMagic.Scene({
 
 
 $(".metier-list--dot").each(function(){
-	var tween = TweenMax.from(this, 1, 
+	var tween = TweenMax.from(this, 0.5, 
 	{scale:0 , duration:200, ease:Power2.easeOut  });
     
 	var scene = new ScrollMagic.Scene({
 		triggerElement: this,
-		triggerHook : 0.85,
+		triggerHook : 0.9,
     	reverse : false,
-	})
-	
+	})	
 	.setTween(tween)
 	.addTo(controller)
 })
@@ -131,12 +134,12 @@ $('.metier-list--title').mouseenter(function(){
 
 
 // BLOCK IMG (CACH) 
-var tween = TweenMax.from("#verticale-img-block .cach-image",0.4,
+var tween = TweenMax.from("#verticale-img-block .cach-image",0.5,
 	{scaleY:"100%"},0});
 			  
 var scene = new ScrollMagic.Scene({
 	triggerElement: "#verticale-img-block",
-	triggerHook : 0.9,
+	triggerHook : 0.8,
 	reverse:true
 })     
 .setTween(tween) 
@@ -150,13 +153,13 @@ var scene = new ScrollMagic.Scene({
 	
 $('.image-project').each(function(){ 
 	var tween = TweenMax.staggerFromTo(this, 1, 
-	{xPercent: 15, scale:1.3}, {xPercent: -15, scale:1.3});
+	{xPercent: 10, scale:1.2}, {xPercent: -10, scale:1.2});
 
 	var scene = new ScrollMagic.Scene({
   		triggerElement: this,
     		triggerHook : 1,
     		reverse : true,
-      		duration: windowHeight*1.4
+      		duration: getwindowHeight(1.4),
   	}) 
     .setTween(tween)
 	.addTo(controller)
@@ -169,14 +172,14 @@ $(".project-item.link-box").each(function(){
 
 	var timeline = new TimelineMax();
     
-   	 var tween0 = TweenMax.from(titleProject,
-   	{ scaleX:1, duration:0.4, ease: Power3.easeOut});
+   	var tween0 = TweenMax.from(titleProject,
+   		{ scaleX:1, duration:0.4, ease: Power3.easeOut});
     
-    	var tween1 = TweenMax.from(infoProject,
-   	{ opacity:0, duration:0.4, ease: Power3.easeOut});
+    var tween1 = TweenMax.from(infoProject,
+   		{ opacity:0, duration:0.4, ease: Power3.easeOut});
       
 	var tween2 = TweenMax.from(cachImg,
-    	{scaleY:1, duration:0.6, ease: Power3.easeOut});
+    	{scaleY:1, duration:0.5, ease: Power3.easeOut});
       
  	timeline.
     	add(tween1,0.4)
@@ -185,7 +188,7 @@ $(".project-item.link-box").each(function(){
 
 	var scene = new ScrollMagic.Scene({
 		triggerElement: this, 
-		triggerHook : 0.9,
+		triggerHook : 0.8,
 		reverse : false,
 	})
 	.setTween(timeline)
@@ -223,9 +226,9 @@ $('.actu-item').each(function(){
   		triggerElement: this,
     		triggerHook : 1,
     		reverse : true,
-      		duration: windowHeight*1.4
+      		duration: getwindowHeight(1.4)
   	}) 
-    	.setClassToggle(this, "active") 
+    .setClassToggle(this, "active") 
 	.addTo(controller)
 })
 
@@ -251,7 +254,7 @@ $('[parallax-anim]').each(function(){
 
 	var scene = new ScrollMagic.Scene({
   		triggerElement: this, 
-    	duration: windowHeight*1.5,
+    	duration: getwindowHeight(1.5),
     	triggerHook : 1,
     	reverse : true,
     	offset:0
@@ -291,14 +294,14 @@ $('.block-text.anim1').each(function(){
     	var tween1 = TweenMax.from(paragraphIntro, 0.3,
    	{ opacity:0, ease: Expo.easeIn});
       
-	var tween2 = TweenMax.from(paragraph, 0.3,
-    	{opacity:0, ease: Expo.easeIn});
+		var tween2 = TweenMax.from(paragraph, 0.3,
+    {opacity:0, ease: Expo.easeIn});
     
     	var tween3 = TweenMax.from(titleBlockLetter, 0.3,
-    	{x:150, stagger:0.02, opacity:0, ease:Power2.easeOut});
+    {x:150, stagger:0.02, opacity:0, ease:Power2.easeOut});
     
      	var tween4 = TweenMax.from(linkBlock, 0.4,
-    	{scale:0, ease: Power3.easeOut});
+    {scale:0, ease: Power3.easeOut});
       
  	timeline
 	.add(tween0,0)
